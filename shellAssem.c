1dc82bbc968ad97afe119fa1e64cfe265a886ebd
rajinish0 <rajinishaneel@gmail.com>
2024-06-25 23:19:35 +0200

    update comments

--- shellAssem.c
+++ shellAssem.c
@@ -6,7 +6,7 @@ void main(){
 	__asm__(
 		"jmp call_label;" //jump to call
 		"pop_label:		;"
-		"popq %rbx;" //call wil jump to here, making eip the addr of string, which will be on the stack so I can just pop it - esi now has address of bin/sh string.
+		"popq %rbx;" //call wil jump to here, making rip the addr of string, which will be on the stack so I can just pop it - rbx now has address of bin/sh string.
 		"movq %rbx,0x8(%rbx);" //move the address to 8 bytes after whatever rbx is pointing to it's pointing to a string with 7 chars with one \0 char at the end so 8 bytes in total
 		"movb $0x0,0x7(%rbx);" //make the last byte 0 just in case
 		"movq $0x0,0x10(%rbx);" // so we have 8 bytes of string, and 8 bytes of address in total the offset is 16 bytes for the NULL val that we need to pass to execve
